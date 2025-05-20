cde5290e26a522d7bf9ff409158cb723f3912fff
Maicon Alves <eumaicondouglas.md45@gmail.com>
2025-05-20 20:44:05 -0300

    Feat: About page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import About from '../components/About'
+import HomeNavbar from '../components/HomeNavbar'
+
+export default function AboutPage() {
+    return(
+        <div>
+            <HomeNavbar/>
+            <About/>
+        </div>
+    )
+}
